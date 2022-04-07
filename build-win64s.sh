b586f55ca7e97d53c43123dcb09006c320f4f874
Richard Franks <git@rf152.co.uK>
2022-04-07 15:19:26 +0100

    Initial Commit

---
+++ build-win64s.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd /app
+mkdir build
+mkdir release
+cd build
+x86_64-w64-mingw32.static-cmake .. -DQT_CMAKE_DIR=/opt/mxe/usr/x86_64-w64-mingw32.static/qt5/lib/cmake/ -DCMAKE_INSTALL_PREFIX=/app/release/
+make -j $(nproc)
+make install
+cd /app
+rm -rf build
